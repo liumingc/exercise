d313ac33190a23a56cf211885585ef41070585ef
liumingc <inferno_ch@163.com>
2019-01-29 18:01:51 +0800

    save temp work

---
+++ parser/parserc.jl
@@ -0,0 +1,102 @@
+
+struct State
+    # run :: tokens -> expr, tokens'
+    run::Function
+end
+
+# bind :: m a -> (a -> m b) -> m b
+function bind(s::State, f::Function)
+    State(tks1 -> begin
+        ex, tks2 = s.run(tks1)
+        # f(ex) :: State
+        f(ex).run(tks2)
+    end)
+end
+
+function seq(s1::State, s2::State)::State
+    #=
+    State(tks -> begin
+        e1, tks1 = s1.run(tks)
+        e2, tks2 = s2.run(tks1)
+        return e2, tks2
+    end)
+    =#
+    State(tks -> bind(s1, e1 -> State(tks1 ->
+        s2.run(tks1)
+    )))
+end
+
+kwtbl = Dict(
+    "if" => :if,
+    "elseif" => :elseif,
+    "else" => :else,
+    "begin" => :begin,
+    "end" => :end
+)
+
+function skip_space(text::AbstractString)
+    i = 1
+    while i < length(text)
+        if !isspace(text[i])
+            break
+        end
+        i += 1
+    end
+    return nothing, text[i:end]
+end
+
+
+function word(text::AbstractString)
+    _, text = skip_space(text)
+    ch = text[1]
+    if ch == '('
+        return "(", text[2:end]
+    elseif ch == ')'
+        return ")", text[2:end]
+    elseif ch == ';'
+        return ";", text[2:end]
+    end
+    i = 1
+    while i < length(text)
+        if isspace(text[i])
+            i -= 1
+            break
+        end
+        i += 1
+    end
+    return text[1:i], text[i:end]
+end
+
+
+function tokenize(text::AbstractString)
+    lst = []
+    while true
+        wd, rest = word(text)
+        push(lst, wd)
+        if length(rest) == 0
+            break
+        end
+    end
+    return lst
+end
+
+function myparse(text::AbstractString)
+    #toks = split(text)
+    toks = tokenize(text)
+    println("=toks=")
+    println(toks)
+end
+
+function main()
+    res = myparse("""
+    if a < 5
+        println("small")
+    elseif a < 10
+        println("media")
+    else
+        println("big")
+    """)
+    println("parsing result: ", res)
+end
+
+main()
